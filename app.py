3492150ec18240b6c88c9dc89bcfcd229bbb1ab7
Munna Das <dasmrpmunna93411@gmail.com>
2025-03-11 16:15:32 +0530

    Pushing  commit

---
+++ app.py
@@ -0,0 +1,34 @@
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+
+# Initialize Flask app
+app = Flask(__name__)
+
+# Load the trained model
+model_path = 'model.pkl'
+with open(model_path, 'rb') as file:
+    model = pickle.load(file)
+
+
+# Home route
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Extract data from form
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    
+    # Make prediction
+    prediction = model.predict(final_features)
+    output = 'Placed' if prediction[0] == 1 else 'Not Placed'
+
+    return render_template('index.html', prediction_text='Prediction: {}'.format(output))
+
+# Run the app
+if __name__ == "__main__":
+    app.run(debug=True)
